c4879ac4424ec564c96369047b59be50cab9a29e
jmej <jmejia@anestheticaudio.com>
2021-05-06 17:47:28 -0700

    p5 broken / in process

--- p5js/sketch.js
+++ p5js/sketch.js
@@ -1,39 +1,41 @@
 
 let img;
 let cam;
-let pixelSize;
+let pixelSize = 4;
 let frames;
 let magnitude = 10;
 let rampSpeed = 0.2;
 let frequency = 10;
 
 function setup() {
-  size(390, 240);
+  createCanvas(390, 240);
   cam = createCapture(VIDEO);
   cam.size(390, 240);
-  pixelSize = 4;
-  imageMode(CENTER);
+  cam.hide();
+  //imageMode(CENTER);
   noStroke();
   background(255);
 }
 
 function draw() {
-
+  background(0);
   frames += rampSpeed;
-    
- 
-  
+  //image(cam, 0, 0, 390, 240);  
+  cam.loadPixels();
   frequency = map(mouseX, 0, width, 1, 10);
   magnitude = map(mouseY, 0, height, 1, 50);
 
-  for (let x = 0; x < width; x += pixelSize){
-    for(let y = 0; y < height; y += pixelSize){
-      color pix = cam.get(x+pixelSize/2, y+pixelSize/2);
-      fill(pix, 128);
+  for (let x = 0; x < width; x += 1){
+    for(let y = 0; y < height; y += 1){
+      let index = 4 * x * y;
+      let r = cam.pixels[index];
+      let g = cam.pixels[index+1];
+      let b = cam.pixels[index+2];
+      fill(r, g, b);
       let phase = map(x, 0, width, 0, frequency);
       let wave = sin(frames-phase);
       wave = wave * magnitude;
-      rect(x, y+wave, pixelSize, pixelSize);
+      rect(x, y+wave, 1, 1);
     }
   }
   
